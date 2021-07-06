94916e7effbbd177d39b9f7e67b18cacab8d1b95
Antonio06Martins <toni6martins@gmail.com>
2021-07-06 20:28:52 -0300

    feature: development mode

---
+++ src/main/kotlin/com/antonio/apirest/config/security/DevSecurityConfigurations.kt
@@ -0,0 +1,20 @@
+package com.antonio.apirest.config.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+
+@EnableWebSecurity
+@Configuration
+@Profile("dev")
+class DevSecurityConfigurations : WebSecurityConfigurerAdapter() {
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+            .antMatchers("/**").permitAll()
+            .and().csrf().disable()
+    }
+}
